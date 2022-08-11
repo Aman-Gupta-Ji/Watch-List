9d4ec4f3e4680c41a396e6f7bf5e15ececf15589
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-08-11 17:25:44 +0530

    async & await

--- src/App.js
+++ src/App.js
@@ -6,22 +6,18 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.empisode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releseDate: movieData.relese_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.empisode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releseDate: movieData.relese_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
 
   return (
